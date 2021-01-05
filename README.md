e9891a9df598efc358905a69fa994ea86c0a5837
Chonli <Chonli@users.noreply.github.com>
2021-01-05 19:05:34 +0100

    import initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# game_counter
+
+A game counter en Flutter
+
+
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:game_counter/ui/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Game couter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
